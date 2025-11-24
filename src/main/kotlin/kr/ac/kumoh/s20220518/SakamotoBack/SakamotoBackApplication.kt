7f0e150add2b14755995a31527029e3d8c48900d
benggi06 <rldnd0616@gmail.com>
2025-11-24 14:30:30 +0900

    Project Start

---
+++ src/main/kotlin/kr/ac/kumoh/s20220518/SakamotoBack/SakamotoBackApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220518.SakamotoBack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SakamotoBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<SakamotoBackApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220518/SakamotoBack/SakamotoBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220518.SakamotoBack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SakamotoBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
